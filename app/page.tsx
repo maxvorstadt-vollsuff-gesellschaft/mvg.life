4782d009e7952056c1aedb95412051889c59f5e7
timkn <timknothe21@gmail.com>
2024-06-08 18:04:31 +0200

    - added spotify - design changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>
                 <h1 className="font-bold text-4xl text-amber-800">MVG</h1>
-                <p className="text-gray-500 mb-8">Maxvorstadt Gang</p>
-                <p className="mb-8">Heute Saufen bei Jonas auf der Terasse - Start 8 Uhr</p>
+                <p className="text-gray-500 mb-4">Maxvorstadt Gang</p>
+                <p className="mb-8">Heute Saufen bei Jonas auf der Terasse. Start 8 Uhr!</p>
 
                 <Countdown date={new Date("2024-06-08T20:00:00")}/>
-                <p className="text-center m-2 mb-8 text-2xl font-bold text-amber-800">Attacke!</p>
+                <p className="text-center mb-8 text-2xl font-semibold text-amber-800">Attacke</p>
 
                 <Image
                     src={image1}
@@ -24,7 +24,7 @@ export default function Home() {
                     // placeholder="blur" // Optional blur-up while loading
                 />
 
-                <ul className="font-mono text-amber-800">
+                <ul className="font-mono text-amber-800 mb-8">
                     <li>Jonas</li>
                     <li>Jonny (Schwabe)</li>
                     <li>Linus</li>
@@ -32,6 +32,12 @@ export default function Home() {
                     <li>Tim</li>
                     <li>Tom</li>
                 </ul>
+
+
+
+                <a href="https://open.spotify.com/playlist/21pJxURnxxWy8Lpfk8KqqU?si=718f4c5f17c34b46"
+                   className="text-center font-mono text-emerald-700">Spotify</a>
+
 
             </div>
         </main>
