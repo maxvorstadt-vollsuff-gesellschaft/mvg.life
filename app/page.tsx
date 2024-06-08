a1cbc032e9eee9e229d85e1a38c148123416ebde
timkn <timknothe21@gmail.com>
2024-06-08 16:38:15 +0200

    countdown v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Countdown from "@/app/Countdown";
 
 export default function Home() {
     return (
@@ -7,6 +8,8 @@ export default function Home() {
                 <h1 className="font-bold text-4xl text-blue-700">MVG</h1>
                 <p className="text-gray-500">Maxvorstadt Gang</p>
                 <p className="mt-8">Heute Saufen bei Jonas auf der Terasse. Start 8 Uhr</p>
+
+                <Countdown  date={new Date("2024-06-08T20:00:00")} />
             </div>
         </main>
     );
