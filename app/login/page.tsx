6c843ec2dc97cac5c79a09731edd05e1e74e83ca
timkn <timknothe21@gmail.com>
2024-06-27 20:28:07 +0200

    fixed login and implemneted logout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,56 +1,81 @@
-'use client'
+"use client";
 
-import {FormEvent} from 'react'
-import {redirect} from "next/navigation";
+import { FormEvent } from "react";
+import { redirect } from "next/navigation";
 import axios from "axios";
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
-import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import Link from "next/link";
+import { navigate } from "../utils";
 
 export default function LoginPage() {
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
-    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault()
+    const formData = new FormData(event.currentTarget);
+    const username = formData.get("username");
+    const password = formData.get("password");
 
-        const formData = new FormData(event.currentTarget)
-        const username = formData.get('username')
-        const password = formData.get('password')
+    // @ts-ignore
+    axios
+      .postForm("https://api.mvg.life/token", {
+        username: username,
+        password: password,
+        grant_type: "",
+        scope: "",
+        client_id: "",
+        client_secret: "",
+      })
+      .then((response) => {
+        console.log(response.data);
+        localStorage.setItem("token", response.data.access_token);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
-
-        // @ts-ignore
-        axios.postForm('https://api.mvg.life/token', {username: username, password: password, grant_type: "", scope: "", client_id: "", client_secret: ""})
-            .then(response => {
-                console.log(response.data);
-                localStorage.setItem('token', response.data.access_token)
-                redirect('/')
-
-            })
-            .catch(error => {
-                console.error(error);
-            });
-
-
-    }
-
-    return (
-        <div className="flex min-h-screen flex-col items-center justify-between px-24 py-12 bg-amber-100">
-            <Card>
-                <CardHeader>
-                    <CardTitle>MVG Login</CardTitle>
-                    <CardDescription>Login with your credentials</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <form onSubmit={handleSubmit} className="block">
-                        <Input type="text" className="block mb-4" name="username" placeholder="Username" required/>
-                        <Input type="password" className="block mb-4" name="password" placeholder="Password" required/>
-                        <Button variant="outline">Login</Button>
-                    </form>
-                </CardContent>
-                <CardFooter>
-                    <Link className="font-mono text-amber-800" href="/">[go Home]</Link>
-                </CardFooter>
-            </Card>
-        </div>
-    )
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between px-24 py-12 bg-amber-100">
+      <Card>
+        <CardHeader>
+          <CardTitle>MVG Login</CardTitle>
+          <CardDescription>Login with your credentials</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit} className="block">
+            <Input
+              type="text"
+              className="block mb-4"
+              name="username"
+              placeholder="Username"
+              required
+            />
+            <Input
+              type="password"
+              className="block mb-4"
+              name="password"
+              placeholder="Password"
+              required
+            />
+            <Button variant="outline">Login</Button>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <Link className="font-mono text-amber-800" href="/">
+            [go Home]
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,77 +1,98 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import Countdown from "@/app/Countdown";
-import image1 from '../public/image1.jpg'
-import {useEffect, useState} from "react";
+import image1 from "../public/image1.jpg";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Link from "next/link";
 
 type Member = {
-    name: string;
-    id: number;
+  name: string;
+  id: number;
 };
-
 
 type Event = {
-    id: number;
-    name: string;
-    start_time: string;
-    participants: Member[];
+  id: number;
+  name: string;
+  start_time: string;
+  participants: Member[];
 };
 
-
 export default function Home() {
-    const [nextEvents, setNextEvents] = useState<Event[]>([]);
+  const [nextEvents, setNextEvents] = useState<Event[]>([]);
+  const [token, setToken] = useState<string | null>(null);
 
-    useEffect(() => {
-        axios.get('https://api.mvg.life/upcoming_events')
-            .then(response => {
-                setNextEvents(response.data);
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }, []);
+  useEffect(() => {
+    setToken(localStorage.getItem("token"));
+    axios
+      .get("https://api.mvg.life/upcoming_events")
+      .then((response) => {
+        setNextEvents(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
+  return (
+    <main className="px-12 py-6 sm:px-24 sm:py-12">
+      <div>
+        <h1 className="font-bold text-4xl text-amber-800">MVG</h1>
+        <p className="text-gray-500 mb-1">Maxvorstadt Gang</p>
 
+        <div className="mb-4">
+          <Link className="font-mono text-amber-800" href="/events">
+            [Events]
+          </Link>
 
-    return (
-        <main className="px-12 py-6 sm:px-24 sm:py-12">
-            <div>
-                <h1 className="font-bold text-4xl text-amber-800">MVG</h1>
-                <p className="text-gray-500 mb-1">Maxvorstadt Gang</p>
+          {token ? (
+            <button
+              className="font-mono text-cyan-950"
+              onClick={() => {
+                localStorage.removeItem("token");
+                setToken(null);
+              }}
+            >
+              [Logout]
+            </button>
+          ) : (
+            <Link className="font-mono text-cyan-950" href="/login">
+              [Login]
+            </Link>
+          )}
 
-                <div className="mb-4">
-                    <Link className="font-mono text-amber-800" href="/events">[Events]</Link>
-                    <Link className="font-mono text-cyan-950" href="/login">[Login]</Link>
+          <a
+            href="https://open.spotify.com/playlist/21pJxURnxxWy8Lpfk8KqqU?si=718f4c5f17c34b46"
+            className="font-mono text-emerald-700 mb-2 "
+          >
+            [Spotify]
+          </a>
+        </div>
+        <p className="mb-4">{nextEvents[0]?.name || "no upcoming events"}</p>
 
-                    <a href="https://open.spotify.com/playlist/21pJxURnxxWy8Lpfk8KqqU?si=718f4c5f17c34b46"
-                       className="font-mono text-emerald-700 mb-2 ">[Spotify]</a>
-                </div>
-                <p className="mb-4">{nextEvents[0]?.name || "no upcoming events"}</p>
+        <Countdown
+          date={new Date(nextEvents[0]?.start_time || "2024-06-10T12:00:00")}
+        />
 
-                <Countdown date={new Date(nextEvents[0]?.start_time || "2024-06-10T12:00:00")}/>
+        <Image src={image1} alt="Picture of the MVG Gang" className="my-8" />
 
-                <Image
-                    src={image1}
-                    alt="Picture of the MVG Gang"
-                    className="my-8"
-                />
+        <ul className="font-mono text-amber-800 mb-8">
+          <li>Jonas</li>
+          <li>Jonny (Schwabe)</li>
+          <li>Linus</li>
+          <li>Philipp</li>
+          <li>Tim</li>
+          <li>Tom</li>
+        </ul>
 
-                <ul className="font-mono text-amber-800 mb-8">
-                    <li>Jonas</li>
-                    <li>Jonny (Schwabe)</li>
-                    <li>Linus</li>
-                    <li>Philipp</li>
-                    <li>Tim</li>
-                    <li>Tom</li>
-                </ul>
-
-
-                <a href="https://github.com/timkn/mvg.life"
-                   className="font-mono text-gray-700 ">GitHub</a>
-            </div>
-        </main>
-    );
+        <a
+          href="https://github.com/timkn/mvg.life"
+          className="font-mono text-gray-700 "
+        >
+          GitHub
+        </a>
+      </div>
+    </main>
+  );
 }
